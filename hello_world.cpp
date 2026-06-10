100f6932dd602ac06814e606470ff10e2bcbcd90
ponomaryovmiron <godgezuru@gmail.com>
2026-06-10 10:24:51 +0000

    update hello world greeting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    string name;
+    cin >> name;
+
+    cout << "Hello world from @" << name << endl;
+
     return 0;
 }
