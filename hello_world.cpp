d974374f9f5292c3d248a3a83feab08ee428dc3f
ponomaryovmiron <godgezuru@gmail.com>
2026-06-10 10:29:12 +0000

    add comments to hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,11 @@
 
 int main()
 {
+    // Read user name from standard input.
     std::string name;
     std::cin >> name;
 
+    // Print greeting message.
     std::cout << "Hello world from @" << name << std::endl;
 
     return 0;
